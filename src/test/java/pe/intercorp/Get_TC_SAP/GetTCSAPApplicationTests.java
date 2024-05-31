bf82483e29dfe61a739bd4d30faaf33f98826d48
JuanLuey <jcluey@hotmail.com>
2024-05-31 15:39:57 -0500

    feat: commit

--- src/test/java/pe/intercorp/Get_TC_SAP/GetTCSAPApplicationTests.java
+++ src/test/java/pe/intercorp/Get_TC_SAP/GetTCSAPApplicationTests.java
@@ -1,10 +1,10 @@
-package pe.intercorp.ValidateDad;
+package pe.intercorp.Get_TC_SAP;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ValidateDadApplicationTests {
+class GetTCSAPApplicationTests {
 
 	@Test
 	void contextLoads() {
