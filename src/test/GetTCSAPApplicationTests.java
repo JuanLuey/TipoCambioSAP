c484fe0cf6bbc551f599f3bfcf8c05f184682351
JuanLuey <126190959+JuanLuey@users.noreply.github.com>
2024-05-31 15:42:28 -0500

    Add files via upload

---
+++ src/test/GetTCSAPApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.intercorp.Get_TC_SAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetTCSAPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
